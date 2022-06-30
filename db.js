3d9433590b68dd3fe5ac08d797ceb447c12225e2
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2022-06-29 23:48:14 -0300

    Fix db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('crud_node','root', 'root'{
+const db = new Sequelize('crud_node','root', 'root',{
     host:"localhost",
     dialect:"mysql"
 });
