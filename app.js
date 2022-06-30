5cf1af63fcef819c78c25020cca67b9fb4047509
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2022-06-30 02:45:10 -0300

    Fix app.js

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
 const port = process.env.PORT|| 5000;
+const router = require('./src/routes/index')
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
+app.use('/', router);
 app.engine('hbs', exphbs.engine({extname:".hbs"}));
 app.set('view engine', 'hbs');
 
-app.get('/', (req,res)=>{
-    res.render('home.hbs');
-})
+
 
 app.listen(port, ()=>{
     console.log(`The server is listening on port ${port}`)
